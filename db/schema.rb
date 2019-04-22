ade80c304a6cb2b3015d6482e67a033071b76f6e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-22 10:17:22 -0700

    switching to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_15_232653) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
